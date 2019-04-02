17929f20d8ebf38c89d9aa660e0d63bc0eb355df
apolalca <apolalca@everis.com>
2019-04-02 21:51:33 +0200

    Task depends of build

--- plugin/lfruploader/src/main/java/com/everis/uploader/UploaderPlugin.java
+++ plugin/lfruploader/src/main/java/com/everis/uploader/UploaderPlugin.java
@@ -2,14 +2,16 @@ package com.everis.uploader;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.Task;
 
 public class UploaderPlugin implements Plugin<Project> {
 
     @Override
     public void apply(Project target) {
-        target.getTasks().create("lfruploader", UploaderTask.class, (task) -> {
+        Task plugin = target.getTasks().create("lfruploader", UploaderTask.class, (task) -> {
             task.setLogger(target.getProject().getLogger());
             task.setFileBuild(target.getBuildDir());
         });
+        plugin.dependsOn("build");
     }
 }
